8aff6006cdc09a0bff59c32bda24e890812b4e34
ing9990 <gimgau0218@naver.com>
2023-03-18 23:43:13 +0900

    Initial commit

---
+++ src/main/java/com/example/cicdtestapp/CicdTestAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.cicdtestapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CicdTestAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CicdTestAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/cicdtestapp/CicdTestAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cicdtestapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CicdTestAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
