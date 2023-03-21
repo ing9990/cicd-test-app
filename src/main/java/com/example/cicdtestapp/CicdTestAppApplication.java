443b4a3e359e980c2f2ec411a435b4deac8e9359
ing9990 <gimgau0218@naver.com>
2023-03-21 11:23:16 +0900

    Commit for PollSCM Trigger

--- src/main/java/com/example/cicdtestapp/CicdTestAppApplication.java
+++ src/main/java/com/example/cicdtestapp/CicdTestAppApplication.java
@@ -2,12 +2,17 @@ package com.example.cicdtestapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class CicdTestAppApplication {
 
+    @Bean
+    public String hellworld(){
+        return "helloworld";
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(CicdTestAppApplication.class, args);
     }
-
 }
